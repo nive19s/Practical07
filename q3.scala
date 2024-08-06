5ca90489d47435e45fa8918c671fd89f4ce6b629
nive19s <sothilingamnivethan19@gmail.com>
2024-08-06 10:53:14 +0530

    q3 added

--- q3.scala
+++ q3.scala
@@ -5,8 +5,6 @@ object PrimeFilter{
             case 2 => true
             case n if n%2 == 0 => false
             case _ => !(3 to math.sqrt(num).toInt by 2).exists(num % _ == 0)
-          //This generates odd numbers starting from 3 up to the integer value of the square root of num
-    //exists(i => num % i == 0) checks if any of these numbers can divide num without a remainder. If such a divisor exists, num is not prime.
         }
     }
 
